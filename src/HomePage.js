e510c7004ab5c4f3ace2f60bc4fb85646a6a3513
leocthu <leocthu@gmail.com>
2023-07-26 15:38:30 -0700

    added confirmation to homepage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,6 +5,7 @@ import 'datatables.net';
 import 'datatables.net-dt';
 import imageSrc from './BodyReference.jpeg';
 import allevamedicallogo from './allevamedicallogo.png';
+import dicut from './dicut.jpg';
 
 import { ref, push } from 'firebase/database';
 import { auth, database } from './firebase';
@@ -13,7 +14,7 @@ function TableComponent() {
   const [nurseName, setNurseName] = useState('');
   const [orderDate, setOrderDate] = useState('');
   const [compName, setCompName] = useState('');
-  const [orderNum, setOrderNum] = useState('');
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   useEffect(() => {
     $(document).ready(function() {
@@ -58,7 +59,7 @@ function TableComponent() {
       console.log('User not authenticated');
       return;
     }
-    const uniqId = `${compName}-${nurseName}-${orderDate}-${orderNum}`;
+    const uniqId = `${compName}-${nurseName}-${orderDate}`;
     const userId = user.uid;
     // Get a reference to the 'orders' node in the Firebase Realtime Database
     const ordersRef = ref(database, `users/${userId}/orders/${uniqId}`);
@@ -80,14 +81,22 @@ function TableComponent() {
         setCompName(''); // Reset the input variables
         setNurseName('');
         setOrderDate('');
-        setOrderNum('');
 
       })
       .catch((error) => {
         // Handle errors if any
         console.error('Error saving data:', error);
       });
+      setShowConfirmation(false);
   };
+
+  const handleBackBtn = () => {
+    setShowConfirmation(false);
+  }
+
+  const handleConfirmBtn = () => {
+    setShowConfirmation(true);
+  }
 
   const handleClearBtn = () => {
     // Create a new array with empty input values for each row
@@ -96,6 +105,9 @@ function TableComponent() {
       userInput1: '',
       userInput2: '',
     }));
+    setCompName('');
+    setOrderDate('');
+    setNurseName('');
 
     // Update the state with the cleared data
     setTableData(clearedData);
@@ -104,96 +116,137 @@ function TableComponent() {
 
 
 
+
   return (
     <div>
       <header className = "login-header" style={{ backgroundColor: 'lightblue' }}>
           <img src={allevamedicallogo} alt="Logo" className="logo-image" />
       </header>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', marginTop: '10px', backgroundColor: 'light grey'}}>
-        <div id="tableContainer" style={{ marginTop: '200px', marginRight: '70px', paddingRight: '50px'}}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', backgroundColor: 'light grey'}}>
+        <div id="tableContainer" style={{ marginTop: '100px', marginRight: '70px', paddingRight: '50px', paddingTop: '70px'}}>
+          {showConfirmation && <h1>Confirmation Page</h1>}
           <div style={{ display: 'flex', marginBottom: '20px'}}>
-            <div style={{ marginRight: '20px' }}>
-              <label className="company">Company: </label>
-              <input
-                type="text"
-                value={compName}
-                onChange={(e) => setCompName(e.target.value)}
-              />
+          <div style={{ marginRight: '20px' }}>
+              <label className="company">Company:  </label>
+              {showConfirmation ? (
+                <span>{compName}</span> // Non-editable text when confirmed
+              ) : (
+                <input
+                  type="text"
+                  value={compName}
+                  onChange={(e) => setCompName(e.target.value)}
+                />
+              )}
             </div>
             <div style={{ marginRight: '20px' }}>
-              <label className="nurse">Nurse's Name:</label>
-              <input
-                type="text"
-                value={nurseName}
-                onChange={(e) => setNurseName(e.target.value)}
-              />
+              <label className="nurse">Username:  </label>
+              {showConfirmation ? (
+                <span>{nurseName}</span> // Non-editable text when confirmed
+              ) : (
+                <input
+                  type="text"
+                  value={nurseName}
+                  onChange={(e) => setNurseName(e.target.value)}
+                />
+              )}
             </div>
             <div style={{ marginRight: '20px' }}>
-              <label className="date">Date:</label>
-              <input
-                type="date"
-                value={orderDate}
-                onChange={(e) => setOrderDate(e.target.value)}
-              />
-            </div> 
-            <div>
-              <label className="orderNum">Order #:</label>
-              <input
-                type="text"
-                value={orderNum}
-                onChange={(e) => setOrderNum(e.target.value)}
-              />
+              <label className="date">Date:  </label>
+              {showConfirmation ? (
+                <span>{orderDate}</span> // Non-editable text when confirmed
+              ) : (
+                <input
+                  type="date"
+                  value={orderDate}
+                  onChange={(e) => setOrderDate(e.target.value)}
+                />
+              )}
             </div> 
           </div>
           
-          
-          
-          
-          <table style={{ width: '100%' }}>
-            <thead>
-              <tr>
-                <th>Body Number</th>
-                <th>Body Part</th>
-                <th>Left Measurement</th>
-                <th>Right Measurement</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tableData.map((row, index) => (
-                <tr key={row.id}>
-                  <td>{index + 1}</td>
-                  <td>{row.name}</td>
-                  <td>
-                    <input
-                      type="text"
-                      value={row.userInput1}
-                      onChange={(e) => handleInputChange(e, index, 'userInput1')}
-                     
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="text"
-                      value={row.userInput2}
-                      onChange={(e) => handleInputChange(e, index, 'userInput2')}
-                    
-                    />
-                  </td>
+          {showConfirmation ? (
+            <div>
+              
+              <table style={{ width: '100%' }}>
+                <thead>
+                  <tr>
+                    <th>Body Number</th>
+                    <th>Body Part</th>
+                    <th>Left Measurement</th>
+                    <th>Right Measurement</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableData.map((row, index) => (
+                    <tr key={row.id}>
+                      <td>{index + 1}</td>
+                      <td>{row.name}</td>
+                      <td>{row.userInput1}</td>
+                      <td>{row.userInput2}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <table style={{ width: '100%' }}>
+              <thead>
+                <tr>
+                  <th>Body Number</th>
+                  <th>Body Part</th>
+                  <th>Left Measurement</th>
+                  <th>Right Measurement</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {tableData.map((row, index) => (
+                  <tr key={row.id}>
+                    <td>{index + 1}</td>
+                    <td>{row.name}</td>
+                    <td>
+                      <input
+                        type="text"
+                        value={row.userInput1}
+                        onChange={(e) => handleInputChange(e, index, 'userInput1')}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        value={row.userInput2}
+                        onChange={(e) => handleInputChange(e, index, 'userInput2')}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+          <div className="button-container">
+            <button className="clearbtn" onClick={handleClearBtn}>Clear</button>
+            <button className="Confirmbtn" onClick={handleConfirmBtn}>Confirm</button>
+            {showConfirmation && (
+              <button className="Backbtn" onClick={handleBackBtn}>Back</button>
+            )}
+            {showConfirmation && (
+              <button className="Orderbtn" onClick={handleOrderbtn}>Place Order</button>
+            )}
+          </div>
+          
 
-          <button className ="Orderbtn"onClick={handleOrderbtn}>Place Order</button> 
-          <button className ="clearbtn"onClick={handleClearBtn}>Clear</button> 
-
-
+ 
+          
         </div>
-
-
-        <div style={{ marginBottom: '250px' }}>
-          <img src={imageSrc} alt="bodypic" style={{ width: '130%', height: 'auto', paddingTop: '40px', marginRight: '75px'}} />
-        </div>
+        {showConfirmation ? (
+          <div style={{ marginBottom: '250px' }}>
+            <img src={dicut} alt="dicutpic" style={{ width: '110%', height: 'auto', paddingTop: '40px', marginRight: '75px'}} />
+          </div>
+        ) : (
+          <div style={{ marginBottom: '250px' }}>
+            <img src={imageSrc} alt="bodypic" style={{ width: '130%', height: 'auto', paddingTop: '40px', marginRight: '75px'}} />
+          </div>
+        )}
+        
       </div>
     </div>
   );
